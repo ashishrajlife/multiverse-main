15a3b1bfc13079851d72eb1ad908a7bdd8e28c77
Ashish Raj <ashishrajlife@gmail.com>
2026-09-20 20:20:50 +0530

    updates

--- obj/Debug/net10.0/ERPDemo.AssemblyInfo.cs
+++ obj/Debug/net10.0/ERPDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERPDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6776cd43c83db0d61be2d76c3664b88d83e74adf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13ff49ba80a414aa5993f3c1c20254b4adfdf8eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERPDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERPDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
