2d803da388f736c5bed747c751447fe3a4724567
Ashish Raj <ashishrajlife@gmail.com>
2026-09-22 00:04:43 +0530

    dll files

--- obj/Debug/net10.0/ERPDemo.AssemblyInfo.cs
+++ obj/Debug/net10.0/ERPDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERPDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c718d00961deb4c0df3d71c3835091007c7dd30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4fb65d124a444e46fb6fb7f549f2cbd829a0744")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERPDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERPDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
